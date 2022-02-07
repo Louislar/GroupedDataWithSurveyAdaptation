cda8bfb66baa6ac97ae7b901aea85cf5ea09368b
louisLiang <a8945267@gmail.com>
2022-02-07 16:08:50 +0800

    Update estimation_error.py
    列出estimation_error需要計算的內容

--- config_version/estimation_error.py
+++ config_version/estimation_error.py
@@ -1,5 +1,8 @@
 '''
 Compute the error of estimation by the distance metrics mentioned in the article
+1. Mean estimation error(Gamma fit/Midpoint/VAM)(Population/Cohort)
+2. Probability vector estimation error(Linfinity/KL divergence)(Population/Cohort)
+3. Bootstrap analysis 95% CI (.ipynb on healthdb)
 '''
 
 import numpy as np 
