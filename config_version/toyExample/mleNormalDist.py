c891e98c050fb71ec168ded3e7ae08a5eae7da95
louisLiang <a8945267@gmail.com>
2022-11-29 00:13:56 +0800

    Update mleNormalDist.py
    修正toy example的參數, 凸顯VAM的修正是好的/有用的

--- config_version/toyExample/mleNormalDist.py
+++ config_version/toyExample/mleNormalDist.py
@@ -7,21 +7,24 @@ from scipy.optimize import minimize
 import pandas as pd
 
 IntervalSet1 = [
-    [-1e6, 0], 
-    [0, 1e6]
+    [-5, 1], 
+    [1, 5]
 ]
 IntervalSet2 = [
-    [-1e6, -1],
+    [-5, -2],
+    [-2, -1],
     [-1, 1],
-    [1, 1e6]
+    [1, 3], 
+    [3, 5]
 ]
 
-MidpointsSet1 = [-5e5, 5e5]
-MidpointsSet2 = [-75e4, 0, 75e4]
+MidpointsSet1 = [-2, 3]
+MidpointsSet2 = [-3.5, -1.5, 0, 2, 4]
 
-relFreqQ1 = [0.5005, 0.4995]
-relFreqQ2 = [0.3335, 0.3335, 0.3330]
-relFreqVAMAdj = [0.3335, 0.3334, 0.3331]
+relFreqQ1 = [0.6919, 0.3081]
+relFreqQ2 = [0.0227, 0.1359, 0.6826, 0.1573, 0.0013]
+# 中間少0.01, 最左與最右加0.05
+relFreqVAMAdj = [0.0277, 0.1359, 0.6726, 0.1573, 0.0063]
 
 sampleSize = 1e4
 
@@ -50,8 +53,8 @@ def truncatedNormalIntervalLogCDF(dataIn, qInterval, mean, std, lb, ub):
         ## 這邊若是機率為0導致log(機率)為-inf, 
         ## 代表給定的參數產生的distribution在此區間產生樣本的機率為0
         ## 代表這並非我們想要的distribution
-        ## TODO: 但是回傳inf不是個好事, 該如何解決?
-        logProb +=  _freq_i * (np.log(_cProb) if _cProb!=0 else -1e10)
+        ## TODO: 但是回傳-inf不是個好事, 該如何解決?
+        logProb +=  _freq_i * (np.log(_cProb) if _cProb!=0 else -1e5)
     # print(logProb)
     return logProb * (-1)
 
@@ -91,8 +94,8 @@ def main():
         bounds=[(-1e6, 1e6), (-1e6, 1e6)]
     )
     mleQ1 = [mleQ1.x[0], mleQ1.x[1]]
-    print(computeTrunNormMean(mleQ1[0], mleQ1[1], -1e6, 1e6))
-    print(mleQ1)
+    print('Q1 mean: ', computeTrunNormMean(mleQ1[0], mleQ1[1], -1e6, 1e6))
+    print('Q1 param: ', mleQ1)
     ## mle of q2 
     mleQ2 = minimize(
         lambda x: truncatedNormalIntervalLogCDF(
@@ -108,10 +111,36 @@ def main():
         bounds=[(-1e6, 1e6), (-1e6, 1e6)]
     )
     mleQ2 = [mleQ2.x[0], mleQ2.x[1]]
-    print(computeTrunNormMean(mleQ2[0], mleQ2[1], -1e6, 1e6))
-    print(mleQ2)
+    print('Q2 mean: ', computeTrunNormMean(mleQ2[0], mleQ2[1], -1e6, 1e6))
+    print('Q2 param: ', mleQ2)
     ## mle of q1 adjusted by VAM 
+    mleVAMAdj = minimize(
+        lambda x: truncatedNormalIntervalLogCDF(
+            dataIn=freqVAMAdj,
+            qInterval=IntervalSet2,
+            mean=x[0],
+            std=x[1], 
+            lb=-1e6, 
+            ub=1e6
+        ),
+        x0=np.array([1, 1]),
+        method='SLSQP',
+        bounds=[(-1e6, 1e6), (-1e6, 1e6)]
+    )
+    mleVAMAdj = [mleVAMAdj.x[0], mleVAMAdj.x[1]]
+    print('Q1 adjusted by VAM mean: ', computeTrunNormMean(mleVAMAdj[0], mleVAMAdj[1], -1e6, 1e6))
+    print('Q1 adjusted by VAM param: ', mleVAMAdj)
 
+    ## Midpoint result
+    ## Q1 
+    midQ1 = [i*j for i, j in zip(MidpointsSet1, relFreqQ1)]
+    ## Q2 
+    midQ2 = [i*j for i, j in zip(MidpointsSet2, relFreqQ2)]
+    ## Q1 adjust br VAM 
+    midVAMAdj = [i*j for i, j in zip(MidpointsSet2, relFreqVAMAdj)]
+    print('midpoint Q1: ', sum(midQ1))
+    print('midpoint Q2: ', sum(midQ2))
+    print('midpoint Q1 adjusted by VAM: ', sum(midVAMAdj))
     
     pass
 
